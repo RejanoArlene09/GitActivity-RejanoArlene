9c6413ca6fc13d9562c7125e152cced92d6c46db
RejanoArlene <arlenerejano72@gmail.com>
2022-03-27 12:20:39 +0800

    My Educational Background added

--- git-python-rejano.py
+++ git-python-rejano.py
@@ -1,3 +1,6 @@
 print ("Rejano Arlene");
 print ("Hinadharan Mogpog Marinduque");
 print ("February 9, 20002");
+print ("Marinduque State College");
+print ("Bachelor of Science in Information Technology");
+print ("2-F")
