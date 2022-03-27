81743ca76d26a8d6da890c6f7c44381d2da5b2d8
RejanoArlene <arlenerejano72@gmail.com>
2022-03-27 12:30:00 +0800

    My hobbies added

--- git-python-rejano.py
+++ git-python-rejano.py
@@ -4,3 +4,8 @@ print ("February 9, 20002");
 print ("Marinduque State College");
 print ("Bachelor of Science in Information Technology");
 print ("2-F")
+print ("Singing");
+print ("Spoken Writing");
+print ("Modeling");
+print ("Travelling");
+print ("Vlogging");
